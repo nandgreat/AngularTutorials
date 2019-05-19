a48454830e4383f5396267e04ab90fc7d5ec3e2c
nandgreat <paul4nank@gmail.com>
2019-05-19 20:54:28 +0100

    Components

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `<div>
+      Inline Template
+  </div>`,
+  styles: [`
+    div {
+      color: red;
+    }
+  `]
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
