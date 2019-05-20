8a9af4d97c1f4cfb846e0dae59da03638be66c72
nandgreat <paul4nank@gmail.com>
2019-05-20 04:52:28 +0100

    Event Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   <h2>
       Welcome {{name}}
   </h2>
-  <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
-  <h2 [style.color]="highlightColor">Style Binding 2</h2>
-  <h2 [ngStyle]="titleStyle">Style Binding 2</h2>
+
+  <button (click)="onClick($event)">Greet</button>
+  <button (click)="greeting='Welcome Angirmene'">Greet</button>
+  {{greeting}}
+
 
 
   `,
@@ -18,24 +20,16 @@ export class TestComponent implements OnInit {
 
   public name = "Angirmene";
 
-  public successClass = "text-success";
-  public hasError = true;
-
-  public titleStyle = {
-    color: "blue",
-    fontStyle: "italic"
-  }
-
-
-  public highlightColor = 'orange';
+  public greeting = "";
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  greetUser() {
-    return "Hello " + this.name;
+  onClick(event){
+    console.log(event);
+    this.greeting = "Welcome to Nigerian Revolution";
   }
 
 }
