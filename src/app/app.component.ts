01ad1c35318305624f7d00eda0a88056bb02d601
nandgreat <paul4nank@gmail.com>
2019-05-20 11:05:08 +0100

    Component Interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nandom Kumchi';
+  title = 'Nigerian Revolution';
+
+  public name = "Nandom";
+  public message = "";
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `
-
-    <div *ngFor="let color of colors; first as f"><!-- index as i, odd as o, last as l, even as e -->
-      <h2 [style.color]=color>{{f}}:&nbsp;{{color}}</h2>
-    </div>
-
-  
+  template: `  
+    <h2>{{"Hello " + name}}</h2>
+    <button (click)="fireEvent()">Send Event</button>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public colors = ["purple","blue", "green", "red"];
+  @Input('parentData') public name;
 
+  @Output() public childEvent = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  logMessage(value) {
-    console.log(value);
+  fireEvent(){
+    this.childEvent.emit("Hey Nigerian Revolution");
   }
 
 }
