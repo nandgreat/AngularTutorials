c277e5630ea4f79df5c562db587c4edc7ac4ce6f
nandgreat <paul4nank@gmail.com>
2019-05-20 10:07:21 +0100

    ngIf Directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,27 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
+  <!--
   <h2>
       Welcome to my Angular World 
   </h2>
-  <input [(ngModel)]="name" type="text">
-  {{name}}
+  <h3 *ngIf="displayName; else elseBlock">
+    Structural Directives
+  </h3>
+  <ng-template #elseBlock>
+    <h3>
+      Name is hidden
+    </h3>
+  <ng-template>
+  -->
 
+  <div *ngIf="displayName; then thenBlock; else myElseBlock"></div>
+
+  <ng-template #thenBlock>
+    <h3>Nigerian Revolution</h3>
+  </ng-template>
+    
+  <ng-template #myElseBlock>
+    <h3>Hidden</h3>
+  </ng-template>
+  
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name = "";
-
-
+  public displayName = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  logMessage(value){
+  logMessage(value) {
     console.log(value);
   }
 
