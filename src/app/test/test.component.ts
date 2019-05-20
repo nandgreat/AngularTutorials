204a88efd9beb085830707ad8d1d0732b5a6fe28
nandgreat <paul4nank@gmail.com>
2019-05-20 04:41:18 +0100

    Style Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,42 +6,28 @@ import { Component, OnInit } from '@angular/core';
   <h2>
       Welcome {{name}}
   </h2>
+  <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding</h2>
+  <h2 [style.color]="highlightColor">Style Binding 2</h2>
+  <h2 [ngStyle]="titleStyle">Style Binding 2</h2>
 
-  <h2 class="text-success">Nigerian Revolution</h2>
-  <h2 [class]="successClass">Nigerian Revolution</h2>
-  
-  <h2 [class]="successClass" class="text-special">Nigerian Revolution</h2>
-  <h2 [class.text-danger]="hasError">Nigerian Revolution</h2>
-
-  <h2 [ngClass]="messageClasses">Nigerian Revolution</h2>
 
   `,
-  styles: [`
-  .text-success{
-    color: green;
-  }
-  .text-danger{
-    color: red;
-  }
-  .text-special{
-    font-style: italic;
-  }
-  `]
+  styles: []
 })
 export class TestComponent implements OnInit {
 
   public name = "Angirmene";
 
   public successClass = "text-success";
-  public hasError = false;
-  public isSpecial = true;
+  public hasError = true;
 
-  public messageClasses = {
-    "text-success": !this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
+  public titleStyle = {
+    color: "blue",
+    fontStyle: "italic"
   }
 
+
+  public highlightColor = 'orange';
 
   constructor() { }
 
