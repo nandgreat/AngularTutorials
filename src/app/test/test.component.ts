5d61c06858f78517ae4c0a03991dec0b12fdbf4e
nandgreat <paul4nank@gmail.com>
2019-05-20 03:00:09 +0100

    Class Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,32 +6,49 @@ import { Component, OnInit } from '@angular/core';
   <h2>
       Welcome {{name}}
   </h2>
-  <input [id]="myId" type="text" value="Nandom">
-  <input [disabled]="isDisabled" id="{{myId}}" type="text" value="Nandom">
 
+  <h2 class="text-success">Nigerian Revolution</h2>
+  <h2 [class]="successClass">Nigerian Revolution</h2>
+  
+  <h2 [class]="successClass" class="text-special">Nigerian Revolution</h2>
+  <h2 [class.text-danger]="hasError">Nigerian Revolution</h2>
 
-
+  <h2 [ngClass]="messageClasses">Nigerian Revolution</h2>
 
   `,
   styles: [`
-    div {
-      color: red;
-    }
+  .text-success{
+    color: green;
+  }
+  .text-danger{
+    color: red;
+  }
+  .text-special{
+    font-style: italic;
+  }
   `]
 })
 export class TestComponent implements OnInit {
 
-public name = "Angirmene";
-public myId = "testId";
-public isDisabled = false;
-public siteUrl = window.location.href;
+  public name = "Angirmene";
+
+  public successClass = "text-success";
+  public hasError = false;
+  public isSpecial = true;
+
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  greetUser(){
+  greetUser() {
     return "Hello " + this.name;
   }
 
